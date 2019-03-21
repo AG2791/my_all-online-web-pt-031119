d3083f42fe1bd824ef138a74cef63619cee6d502
AJ Guiles <ashiguiles@gmail.com>
2019-03-21 03:40:08 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   i = 0
   block_return_values = {[] 
   while i < collection.length #iterate
-    block_return_values << yield(collection[i]) # #Yield each elemement to the block,then
+    block_return_values << yield(collection[i])  #Yield each elemement to the block,then
  #shovel the elements from block into block_return_value array
     i = i + 1 #increment through loop
   end
