fcefbfb32c32ae0ba320b7284aef6a722111c006
AJ Guiles <ashiguiles@gmail.com>
2019-03-21 03:40:56 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,14 +2,13 @@
 
 def my_all?(collection)
   i = 0
-  block_return_values = {[] 
+  block_return_values =  
   while i < collection.length #iterate
-    block_return_values << yield(collection[i])  #Yield each elemement to the block,then
- #shovel the elements from block into block_return_value array
-    i = i + 1 #increment through loop
+    block_return_values << yield(collection[i])
+    i = i + 1
   end
  
-  if block_return_values.include?(false) #determine return value of method 
+  if block_return_values.include?(false)
     false
   else
     true
